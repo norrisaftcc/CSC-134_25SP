fc3d8e4935a571d635e23fb77f75bd9404277116
teacherbot <norrisa@faytechcc.edu>
2025-01-15 16:36:00 +0000

    added IMO

---
+++ hello.cpp
@@ -0,0 +1,21 @@
+// CSC 134
+// Hello World
+// teacherbot
+// 1/15/25
+
+/* 
+or you can do
+multi line 
+comments
+*/
+// #include lets us use input and output
+#include <iostream>
+// i'll tell you what this does later  
+using namespace std;
+  
+int main()
+{
+   cout << "Hello, CSC 134 Students!" << endl;
+   cout << "Codespaces get easier every time you use them. (imo)";
+   return 0;
+}
