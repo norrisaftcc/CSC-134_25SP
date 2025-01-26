eda14630a234eec9fdccbb843812f42e425a7b3b
teacherbot <norrisa@faytechcc.edu>
2025-01-26 23:50:26 +0000

    m2t1 started

---
+++ m2t1_norris.cpp
@@ -0,0 +1,17 @@
+// CSC 134
+// M2T1
+// norrisa
+// 1/26/2025
+
+
+#include <iostream>
+
+using namespace std;
+  
+int main()
+{
+   cout << "Hello" << endl;
+   cout << "world" << endl;
+   cout << endl;
+   return 0;
+}
