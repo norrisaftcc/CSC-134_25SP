0cbe2abc877e10f88bd43d1ce83bd03cf7744a2b
teacherbot <norrisa@faytechcc.edu>
2025-01-27 16:09:00 +0000

    m1t2 done

---
+++ module1/m1t2_norris.cpp
@@ -0,0 +1,18 @@
+// CSC 134 
+// M1T2 - Hello Students!
+// norrisa
+// 1/27/2025
+
+#include <iostream>
+using namespace std;
+  
+int main()
+{
+   cout << "Hello";
+   cout << "I'm Andrew Norris (norrisaftcc on github)" << endl;
+   cout << "I watch a lot of Japanese tokusatsu shows because they are cool." << endl;
+   // for m1t2, add a few words like who you are
+   // and things you like.
+   cout << endl;
+   return 0;
+}
