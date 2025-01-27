ec08970bedc1dfd784e635ba75f7aaa895680a8f
teacherbot <norrisa@faytechcc.edu>
2025-01-27 18:41:39 +0000

    M1T2 started

---
+++ m1t2_norris.cpp
@@ -0,0 +1,18 @@
+// CSC 134
+// M1T1 - Hello World
+// norrisa
+// 1/27/2025
+
+
+#include <iostream>
+using namespace std;
+  
+int main()
+{
+    cout << "Hello" << endl;
+    cout << endl;
+    // write a few more cout statements
+    // about you and things you like
+    return 0; // no errors
+
+}
