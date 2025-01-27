172c7e41d0d3fe1b104dbf85611c230bb8ffa862
teacherbot <norrisa@faytechcc.edu>
2025-01-27 18:42:17 +0000

    ended m1t2

--- m1t2_norris.cpp
+++ m1t2_norris.cpp
@@ -13,6 +13,9 @@ int main()
     cout << endl;
     // write a few more cout statements
     // about you and things you like
+    cout << "I'm Andrew Norris, I teach at FTCC" << endl;
+    cout << "I watch a lot of tokusatsu like sentai and kamen rider" << endl;
+    
     return 0; // no errors
 
 }
