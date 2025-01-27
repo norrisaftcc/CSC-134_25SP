2aba8079628adb79418c795db3a5f327dc1106d7
teacherbot <norrisa@faytechcc.edu>
2025-01-27 00:02:25 +0000

    M2T1 done!

--- m2t1_norris.cpp
+++ m2t1_norris.cpp
@@ -10,8 +10,24 @@ using namespace std;
   
 int main()
 {
-   cout << "Hello" << endl;
-   cout << "world" << endl;
-   cout << endl;
+    // variables are like mailboxes
+    string name = "Norris";     // replace with your name!
+    int num_apples = 10;        // int are whole numbers, so 3, or 10000000, but not 3.5
+    double price_each = 0.25;   // double stands for floating point, yes that's silly
+                                // double are numbers like 3.50 or 11.00000002
+
+   // think of like an assembly line -- put words or numbers on in the order they should print out.
+   cout << "Welcome to the " << name << " apple farm!" << endl;
+   cout << "We have " << num_apples << " in stock" << endl;
+   cout << "Each apple costs $" << price_each << endl;
+
+   // let's do some math
+   double total_price = num_apples * price_each;
+
+    // finally, we print the answer
+    cout << "The total price for all " << num_apples;
+    cout << " is: $" << total_price << endl;
+    cout << "Thanks for coming by!" << endl;
+
    return 0;
 }
