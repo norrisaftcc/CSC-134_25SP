dd8a8ea7693f52a5043539bfd46cc35fa9c06c6c
teacherbot <norrisa@faytechcc.edu>
2025-01-27 16:38:18 +0000

    almost done

---
+++ module1/m1lab1_norris.cpp
@@ -0,0 +1,24 @@
+// CSC 134 
+// M1Lab1 - The Apple Orchard
+// norris
+// 1/27/2025
+// We're going to make the simplest possible
+// "checkout" machine.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Apple sales program
+    // Variables are like mailboxes
+    string name = "Norris"; // you do whatever name you want
+    int num_apples = 10;    // int are whole numbers: 1, 2, 100000, 42
+    double cost_each = 0.25;// twenty five cents, or $0.25.
+
+    cout << "Welcome to the " << name << " apple farm!" << endl;
+    cout << "There are " << num_apples << " apples in stock." << endl;
+    cout << "They cost $" << cost_each << " each." << endl;
+
+    cout << endl;
+    return 0; // no errors
+}
