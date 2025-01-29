707a624022443825fc0bd355a1130e24917de0b7
teacherbot <norrisa@faytechcc.edu>
2025-01-29 16:20:54 +0000

    m2t1 almost done

--- module2/m2t1_norris.cpp
+++ module2/m2t1_norris.cpp
@@ -4,8 +4,12 @@
 // 1/29/2025
 // We're going to make the simplest possible
 // "checkout" machine.
+// Today's features to add:
+// Ask for user input: name, and how many apples they want
+// Only charge them for that many apples
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main() {
@@ -14,17 +18,28 @@ int main() {
     string farm_name = "Norris"; // you do whatever name you want
     int num_apples = 10;    // int are whole numbers: 1, 2, 100000, 42
     double cost_each = 0.25;// twenty five cents, or $0.25.
+    // new variables for M2T1
+    string user_name;       // Who is buying apples? 
+    int apples_to_buy;      // How many do they want.
 
-    string user_name;
-    int apples_to_buy;
+    // Greet the customer
+    cout << "Hello! What's your name? ";
+    cin >> user_name;
+    cout << "Thanks for coming by, " << user_name << "!" << endl;
 
     cout << "Welcome to the " << farm_name << " apple farm!" << endl;
     cout << "There are " << num_apples << " apples in stock." << endl;
     cout << "They cost $" << cost_each << " each." << endl;
 
+    // This line sets cout for doubles to 2 decimal places
+    // you must put the line #include <iomanip> with the other #include statements up top
+    cout << setprecision(2) << fixed;
+
     // Find out the total price
     double total_cost = num_apples * cost_each;
     cout << "The price for all of them is: $" << total_cost << endl;
+
+    // Now let's make a deal
 
     cout << endl;
     return 0; // no errors
