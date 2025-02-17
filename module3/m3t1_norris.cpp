66e1a7e21cd8f36932f3dfe05da4b1760f7ab053
teacherbot <norrisa@faytechcc.edu>
2025-02-17 15:59:17 +0000

    started m3t1

---
+++ module3/m3t1_norris.cpp
@@ -0,0 +1,29 @@
+// CSC 134
+// M3T1 
+// norrisa
+// 2/17/2025
+// Ask the user for the width and length of two rectangles
+// Find the area
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Variables
+    double len1, wid1, len2, wid2; 
+    double area1, area2;
+
+    // Ask user for the length and width
+    cout << "What's the length of the first rectangle? ";
+    cin >> len1;
+    cout << "What's the width of the first rectangle? ";
+    cin >> wid1;
+    cout << "What's the length of the second rectangle? ";
+    cin >> len2;
+    cout << "What's the width of the second rectangle? ";
+    cin >> wid2;
+
+    // Find the area
+    // Print the area
+    return 0;
+}
