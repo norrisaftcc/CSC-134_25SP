02eacc49919f567f434a0b1a03aa201374301e3d
teacherbot <norrisa@faytechcc.edu>
2025-02-19 14:54:40 +0000

    M3 RNG example

---
+++ module3/m3_random.cpp
@@ -0,0 +1,16 @@
+// CSC 134
+// M3 Random Numbers Example
+// norrisa
+// 2/19/2025
+// Start with dice rolls
+
+#include <iostream>
+// for random
+#include <cmath>    
+#include <ctime>
+using namespace std;
+
+int main() {
+    cout << "Hello!" << endl;
+    return 0;
+}
