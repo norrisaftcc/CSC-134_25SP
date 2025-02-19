696d71bdddc229634bb7d39046ed1b623be866a4
teacherbot <norrisa@faytechcc.edu>
2025-02-19 15:21:57 +0000

    finished m3 bonus rng program

--- module3/m3_random.cpp
+++ module3/m3_random.cpp
@@ -15,22 +15,46 @@ using namespace std;
 int main() {
     cout << "Let's roll some dice!" << endl;
     int seed = time(0);
-    cout << "Your seed is: " << seed << endl;
+    //cout << "Your seed is: " << seed << endl;
     //cout << "What's your lucky number? ";
     //cin >> seed;
     // Seed the RNG
     srand(seed);
 
     const int MAX = 6; // numbers from 1-6
-    int roll;
-    roll  = (rand() % MAX) + 1; // Divide by MAX, and just keep the remainder
-    cout << "Your roll was: " << roll << endl;
+    int roll1, roll2, total;
+    roll1  = (rand() % MAX) + 1; // Divide by MAX, and just keep the remainder
+    cout << "Your roll was: " << roll1 << endl;
 
-    roll  = (rand() % MAX) + 1; // Divide by MAX, and just keep the remainder
-    cout << "Your roll was: " << roll << endl;
+    roll2  = (rand() % MAX) + 1; // Divide by MAX, and just keep the remainder
+    cout << "Your roll was: " << roll2 << endl;
 
-    roll  = (rand() % MAX) + 1; // Divide by MAX, and just keep the remainder
-    cout << "Your roll was: " << roll << endl;
+    total = roll1 + roll2;
+    cout << "Your total roll is: " << total << endl;
+
+    // Let's play craps!
+    // 7 and 11 - win!
+    // 2 and 12 - lose.
+    // anything else -- comes later.
+    if (total == 7) {
+        cout << "Lucky seven! You win!" << endl;
+    }
+    else if (total == 11) {
+        cout << "Eleven is a winner!" << endl;
+    }
+    else if (total == 2) {
+        cout << "Snake eyes! Too bad, you lose." << endl;
+    }
+    else if (total == 3) {
+        cout << "Sorry, three is unlucky, you lose." << endl;
+    }
+    else if (total == 12) {
+        cout << "Boxcars! Sorry, you lost." << endl;
+    }
+    else {
+        // anything else
+        cout << "Your point is " << total << " but we'll do that part later" << endl;
+    }
 
     return 0;
 }
