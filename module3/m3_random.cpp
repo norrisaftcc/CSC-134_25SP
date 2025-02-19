c213c345f762d07348dfdc5b013827e22cc79081
teacherbot <norrisa@faytechcc.edu>
2025-02-19 15:09:39 +0000

    works with set seed

--- module3/m3_random.cpp
+++ module3/m3_random.cpp
@@ -4,13 +4,32 @@
 // 2/19/2025
 // Start with dice rolls
 
+
 #include <iostream>
-// for random
+// for pseudo-random numbers
 #include <cmath>    
+// for making it truly random
 #include <ctime>
 using namespace std;
 
 int main() {
-    cout << "Hello!" << endl;
+    cout << "Let's roll some dice!" << endl;
+    int seed;
+    cout << "What's your lucky number? ";
+    cin >> seed;
+    // Seed the RNG
+    srand(seed);
+
+    const int MAX = 6; // numbers from 1-6
+    int roll;
+    roll  = (rand() % MAX) + 1; // Divide by MAX, and just keep the remainder
+    cout << "Your roll was: " << roll << endl;
+
+    roll  = (rand() % MAX) + 1; // Divide by MAX, and just keep the remainder
+    cout << "Your roll was: " << roll << endl;
+
+    roll  = (rand() % MAX) + 1; // Divide by MAX, and just keep the remainder
+    cout << "Your roll was: " << roll << endl;
+
     return 0;
 }
