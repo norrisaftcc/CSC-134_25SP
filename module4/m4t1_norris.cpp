66d034c4d9ea0adf630e80312c12d1188b5a4a7a
teacherbot <norrisa@faytechcc.edu>
2025-03-10 14:10:30 +0000

    m4t1 started

---
+++ module4/m4t1_norris.cpp
@@ -0,0 +1,20 @@
+// CSC 134
+// M4T1 - While
+// norrisa
+// 3/10/2025
+
+
+#include <iostream>
+using namespace std;
+  
+int main()
+{
+
+    // Part 1, just say "hi" five times
+    int count = 1;
+    while (count < 5) {
+        cout << "Hello number " << count << endl;
+    }
+
+    return 0; // done
+}
