8ed9f54ebf0b3a89fb06e13d2b2dee6b36e313f5
teacherbot <norrisa@faytechcc.edu>
2025-03-10 14:17:22 +0000

    m4t1 part 1

--- module4/m4t1_norris.cpp
+++ module4/m4t1_norris.cpp
@@ -12,9 +12,15 @@ int main()
 
     // Part 1, just say "hi" five times
     int count = 1;
-    while (count < 5) {
+    while (count <= 5) {
         cout << "Hello number " << count << endl;
+        //count = count + 1; 
+        //count += 1;
+        count++; // same as above
     }
+    cout << "Done!" << endl;
 
+
+    // part 2
     return 0; // done
 }
