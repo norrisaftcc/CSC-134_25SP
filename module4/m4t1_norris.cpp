31b2ff294a09646e63b71befe7370d7842ede2f7
teacherbot <norrisa@faytechcc.edu>
2025-03-10 14:45:31 +0000

    optional added to m4t1

--- module4/m4t1_norris.cpp
+++ module4/m4t1_norris.cpp
@@ -37,5 +37,25 @@ int main()
     num = num + 1;
    }
 
+   // OPTIONAL: Example 3
+   bool is_valid = false;
+   int choice;
+   while (is_valid == false) {
+        cout << "Press 1 or 2 to finish." << endl;
+        cin >> choice;
+        if (choice == 1) {
+            cout << "You picked 1" << endl;
+            is_valid = true;
+        }
+        else if (choice == 2) {
+            cout << "You picked 2" << endl;
+            is_valid = true;
+        }
+        else {
+            cout << "Sorry, not valid. Try again." << endl;
+        }
+   }
+   cout << "Goodbye" << endl;
+
     return 0; // done
 }
