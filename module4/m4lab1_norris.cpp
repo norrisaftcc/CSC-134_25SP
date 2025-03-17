d3fe6a7276f8989ec678772498b2c41261e8ced8
teacherbot <norrisa@faytechcc.edu>
2025-03-17 15:19:42 +0000

    adding input validation

--- module4/m4lab1_norris.cpp
+++ module4/m4lab1_norris.cpp
@@ -12,7 +12,9 @@ int main() {
     // New info discovered:
     // any regular character can be char
     // but Unicode characters have to be strings
-    string pixel = "🔥"; // use any one character or an emoji
+    string pixel = "👾"; // use any one character or an emoji
+    const int WIDTH = 8;
+    const int HEIGHT = 5;
     // cout << pixel << endl;
 
     // three ways to run a loop
@@ -33,8 +35,41 @@ int main() {
     // This is the one time using i as a variable name is ok
     // i stands for index
     cout << endl;
-    for (int i = 0; i < 10; i++) {
+    cout << pixel << " printed horizontally (row)" << endl;
+    for (int i = 0; i < WIDTH; i++) {
         cout << pixel << " ";
     }
     cout << endl;
+    cout << pixel << " printed vertically (column)" << endl;
+    for (int i = 0; i < HEIGHT; i++) {
+        cout << pixel << endl;
+    }
+    cout << endl << "Printing a box " << WIDTH << " by " << HEIGHT << endl;
+    // this is a nested loop
+    // for each row
+    for (int h=0; h<HEIGHT; h++) {
+        // print the current row
+        for (int w=0; w<WIDTH; w++) {
+            cout << pixel << " ";
+        }
+        // go to the next line
+        cout << endl;
+    }
+    // just for fun: extra user validation
+    int width, height; // both must be > 0
+    bool good_input = false;
+    do {
+        cout >> "Enter width: ";
+        cin << width;
+        cout >> "Enter height: ";
+        cin << height;
+        if (width > 0 && height > 0) {
+            good_input = true;
+        }
+        else {
+            cout << "Please enter nonzero width and height" << endl;
+        }
+    } while (good_input == false); // repeat until we have valid data
+
+    
 }
