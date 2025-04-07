bcb1fe720935fca0748f73a4accc7c05b5e54e4c
teacherbot <norrisa@faytechcc.edu>
2025-04-07 15:23:46 +0000

    m6t1_0001 finished

--- module6/m6t1_norris.cpp
+++ module6/m6t1_norris.cpp
@@ -20,14 +20,22 @@ int main() {
     int cars_today;
     int cars_total = 0;
     double cars_average;
+    // Add array holding car counts
+    int cars[DAYS];
 
     cout << "Enter cars that pass for each day." << endl;
     for (int i=0; i<DAYS; i++) {
         cout << "Day " << i+1 << ": " ;
         cin >> cars_today;
+        cars[i] = cars_today; // save today's value in the array
         cars_total += cars_today;
     }
     cout << "Over " << DAYS << " days, total cars = " << cars_total << endl;
+    cars_average = (double) cars_total / DAYS; // don't want to round off too eary
+    cout << "For an average of " << cars_average << " each day." << endl;
 
-
+    // Now, print the array
+    for (int i=0; i<DAYS; i++) {
+        cout << "Day: " << i+1 << " Count: " << cars[i] << endl;
+    }
 }
