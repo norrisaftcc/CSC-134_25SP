d876a3acfbf8a786d7e79d2e47ecce55948b66e2
teacherbot <norrisa@faytechcc.edu>
2025-04-07 14:54:28 +0000

    m6t1_initial

---
+++ module6/m6t1_norris.cpp
@@ -0,0 +1,33 @@
+/*
+CSC 134
+M6T1 - Basic Arrays
+norrisa
+4/7/25
+
+For each day Monday through Friday:
+  count the cars that pass the site
+  add them to the total
+Find the total
+Find the average per day
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // First version - no array
+    const int DAYS = 5;
+    int cars_today;
+    int cars_total = 0;
+    double cars_average;
+
+    cout << "Enter cars that pass for each day." << endl;
+    for (int i=0; i<DAYS; i++) {
+        cout << "Day " << i+1 << ": " ;
+        cin >> cars_today;
+        cars_total += cars_today;
+    }
+    cout << "Over " << DAYS << " days, total cars = " << cars_total << endl;
+
+
+}
