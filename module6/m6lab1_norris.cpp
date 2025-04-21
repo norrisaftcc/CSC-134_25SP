61ddf53f600bb7896781b5501239aca60a92f99a
teacherbot <norrisa@faytechcc.edu>
2025-04-21 14:33:49 +0000

    v1 of m6lab

--- module6/m6lab1_norris.cpp
+++ module6/m6lab1_norris.cpp
@@ -7,3 +7,33 @@ norrisa
 
 #include <vector>
 #include <iostream>
+using namespace std;
+
+int main() {
+
+    // Idea is to replicate the simplest parts of Buckshot Roulette.
+    // Variables
+    const int RED = 1;
+    const int BLACK = 2;
+    const int EMPTY = 0; // do we need this? We would with an array
+
+    // example one: Using a basic array
+    int magazine[] = {RED, BLACK, BLACK, RED};
+    for (int shell : magazine) {
+        string shellname;
+        if (shell == BLACK) {
+            shellname = "Black";
+        }
+        if (shell == RED) {
+            shellname = "Red";
+        }
+        cout << "Next shell: " << shellname << endl;
+    }
+
+
+
+
+
+
+    return 0;
+}
