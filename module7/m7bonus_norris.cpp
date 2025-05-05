759bab2507eb5f3a6230cdf7eba1a1c50c9ff3cb
teacherbot <norrisa@faytechcc.edu>
2025-05-05 14:50:53 +0000

    v2 with mario map

--- module7/m7bonus_norris.cpp
+++ module7/m7bonus_norris.cpp
@@ -8,39 +8,45 @@
 using namespace std;
 
 int main() {
-   // Create a new Graph object
-   Graph graph1;
-    
-   // Add vertices and edges representing plane flights
-   Vertex* vertexA = graph1.AddVertex("Tokyo");
-   Vertex* vertexB = graph1.AddVertex("New York");
-   Vertex* vertexC = graph1.AddVertex("London");
-   Vertex* vertexD = graph1.AddVertex("Sydney");
-   graph1.AddUndirectedEdge(vertexA, vertexB, 6743);
-   graph1.AddUndirectedEdge(vertexA, vertexC, 5941);
-   graph1.AddUndirectedEdge(vertexA, vertexD, 4863);
-   graph1.AddUndirectedEdge(vertexB, vertexC, 3425);
-   graph1.AddUndirectedEdge(vertexB, vertexD, 9868);
-   graph1.AddUndirectedEdge(vertexC, vertexD, 10562);
+// Changes by Copilot.
+/*
+prompt: What I would like you to do is: Create one of these maps using this code technique. Here's an example. Weights should be the number of "steps" neeed on the map. You only need to connect, say, three or four important locations.
+
+*/
+// Create a new Graph object for the Super Mario World Level 1 map
+Graph marioMap;
+
+// Add vertices representing key locations on the map
+Vertex* start      = marioMap.AddVertex("Start");
+Vertex* yoshiHouse = marioMap.AddVertex("Yoshi's House");
+Vertex* donutPlains = marioMap.AddVertex("Donut Plains");
+Vertex* castle     = marioMap.AddVertex("Castle");
+
+// Add undirected edges with weights equal to the number of steps required
+marioMap.AddUndirectedEdge(start, yoshiHouse, 5);      // 5 steps from Start to Yoshi's House
+marioMap.AddUndirectedEdge(yoshiHouse, donutPlains, 8);  // 8 steps from Yoshi's House to Donut Plains
+marioMap.AddUndirectedEdge(donutPlains, castle, 12);     // 12 steps from Donut Plains to Castle
+marioMap.AddUndirectedEdge(start, castle, 20);           // 20 steps for a direct (but longer) route from Start to Castle
+
     
    // Show the graph's vertices and edges
-   for (Vertex* vertex : graph1.GetVertices()) {
+   for (Vertex* vertex : marioMap.GetVertices()) {
       cout << "Location: " << vertex->label << endl;
         
       // Show outgoing edges (flights from location)
-      cout << "  Flights from " << vertex->label << ":" << endl;
-      for (Edge* outgoingEdge : *graph1.GetEdgesFrom(vertex)) {
+      cout << "  Trips from " << vertex->label << ":" << endl;
+      for (Edge* outgoingEdge : *marioMap.GetEdgesFrom(vertex)) {
          cout << "   - " << vertex->label << " to ";
          cout << outgoingEdge->toVertex->label << ", ";
-         cout << (int)outgoingEdge->weight << " miles" << endl;
+         cout << (int)outgoingEdge->weight << " steps" << endl;
       }
      
       // Show incoming edges (flights to location)
-      cout << "  Flights to " << vertex->label << ":" << endl;
-      for (Edge* incomingEdge : *graph1.GetEdgesTo(vertex)) {
+      cout << "  Trips to " << vertex->label << ":" << endl;
+      for (Edge* incomingEdge : *marioMap.GetEdgesTo(vertex)) {
          cout << "   - " << incomingEdge->fromVertex->label << " to ";
          cout << vertex->label << ", ";
-         cout << (int)incomingEdge->weight << " miles" << endl;
+         cout << (int)incomingEdge->weight << " steps" << endl;
       }
    }
     
